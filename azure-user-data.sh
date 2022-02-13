b6d1114992ceb82f7a8710ba820ee98686d179c2
Guillermo Musumeci <guillermo-musumeci@users.noreply.github.com>
2022-02-13 09:38:06 +0100

    Initial commit

--- azure-user-data.sh
+++ azure-user-data.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-sudo apt-get update -y
-sudo apt-get install apache2 -y
-sudo systemctl start apache2
-sudo systemctl enable apache2
+sudo yum update -y
+sudo yum install -y httpd
+sudo systemctl start httpd
+sudo systemctl enable httpd
 echo "<h1>Azure Virtual Machine deployed with Terraform</h1>" | sudo tee /var/www/html/index.html
